c409dc0ff3e2c46eba7fe8f9d9ba4eee5d375285
Dipesh Ranjan <sinhadipesh25@gmail.com>
2022-09-17 13:49:50 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+let clock = () => {
+    let date = new Date();
+    let hrs = date.getHours();
+    let mins = date.getMinutes();
+    let secs = date.getSeconds();
+
+    let period = "AM";
+    if (hrs == 0) 
+    {
+        hrs = 12;
+    } else if (hrs >= 12) 
+    {
+         
+    } if (hrs > 12) {
+        hrs = hrs - 12;
+        period = "PM";
+    }
+
+    hrs = hrs < 10 ? "0" + hrs : hrs;
+    mins = mins < 10 ? "0" + mins : mins;
+    secs = secs < 10 ? "0" + secs : secs;
+
+    document.getElementById("clock").innerText = `${hrs} : ${mins} : ${secs} ${period}`;
+    setTimeout(clock, 1000);
+};
+
+clock();
